91b0425334dbc674c5a7af1eb39ac530a1c2b93a
raghuram1004 <kittu.raghu31@gmail.com>
2024-07-10 11:36:49 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull sreedhar8897/hotel-app:latest
+docker pull raghuram9895/javaimage:latest
 
 # Run the Docker image as a container
-docker run -dit -p 80:80 sreedhar8897/hotel-app
+docker run -dit -p 80:80 raghuram9895/javaimage:latest
